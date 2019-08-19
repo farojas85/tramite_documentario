0e12e870249ebd82168d2ceea4e69fbadb429bde
Fredy Rojas <farojas851@gmail.com>
2019-08-18 23:44:30 -0500

    Añadiendo PassportServiceProvider

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Laraveles\Spanish\SpanishServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
         /*
          * Application Service Providers...
          */
