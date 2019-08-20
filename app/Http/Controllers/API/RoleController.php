ec5eefb19353df59b9936494ccdd111a3cb648df
Fredy Rojas <farojas851@gmail.com>
2019-08-20 17:06:00 -0500

    Deshabilitando Authorized User y Role

--- app/Http/Controllers/API/RoleController.php
+++ app/Http/Controllers/API/RoleController.php
@@ -12,7 +12,7 @@ class RoleController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        //$this->middleware('auth:api');
     }
 
     public function index()
